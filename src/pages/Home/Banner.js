5cd5f7d45857d2783f46342124d0c96a48d156ba
zubayer-salehin <freelancerzubear@gmail.com>
2022-09-23 22:23:11 +0600

    newsletter functionlity problame solve

--- src/pages/Home/Banner.js
+++ src/pages/Home/Banner.js
@@ -26,8 +26,10 @@ const Banner = () => {
                 <div className='absolute bannerImagePositionSet'>
                     <h2 className='text-center sm:text-left text-3xl sm:text-5xl font-bold text-primary bannerTitleStyle'>BeautyZone Hair Salon <br /> Creating Beauty</h2>
                     <p className='pt-6 pb-6 hidden sm:block'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, velit <br /> suscipit, a doloribus repellat, cum possimus saepe eaque quidem <br /> quasi ad eligendi dolorem. Voluptatibus, nobis.</p>
-                    <button className='mt-14 sm:mt-0 ml-12 sm:ml-0 px-7 py-3.5 mr-5 bg-primary text-white rounded-sm'>Get A Quote</button>
-                    <button className='px-7 py-3.5 bg-teal-500 text-white rounded-sm'>About Us</button>
+                    <div className='text-center sm:text-left'>
+                        <button className='mt-14 sm:mt-0 px-7 py-3.5 mr-5 bg-primary text-white rounded-sm'>Get A Quote</button>
+                        <button className='px-7 py-3.5 bg-teal-500 text-white rounded-sm'>About Us</button>
+                    </div>
                 </div>
             </SwiperSlide>
             <SwiperSlide className='relative'>
@@ -35,8 +37,10 @@ const Banner = () => {
                 <div className='absolute bannerImagePositionSet'>
                     <h2 className='text-center sm:text-left text-3xl sm:text-5xl font-bold text-primary bannerTitleStyle'>BeautyZone Hair Salon <br /> Creating Beauty</h2>
                     <p className='pt-6 pb-6 hidden sm:block'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, velit <br /> suscipit, a doloribus repellat, cum possimus saepe eaque quidem <br /> quasi ad eligendi dolorem. Voluptatibus, nobis.</p>
-                    <button className='mt-14 sm:mt-0 ml-12 sm:ml-0 px-7 py-3.5 mr-5 bg-primary text-white rounded-sm'>Get A Quote</button>
-                    <button className='px-7 py-3.5 bg-teal-500 text-white rounded-sm'>About Us</button>
+                    <div className='text-center sm:text-left'>
+                        <button className='mt-14 sm:mt-0 px-7 py-3.5 mr-5 bg-primary text-white rounded-sm'>Get A Quote</button>
+                        <button className='px-7 py-3.5 bg-teal-500 text-white rounded-sm'>About Us</button>
+                    </div>
                 </div>
             </SwiperSlide>
         </Swiper>
--- src/pages/Home/NewsLetter.js
+++ src/pages/Home/NewsLetter.js
@@ -1,9 +1,21 @@
 import { faSpa } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
+import { useState } from 'react';
 import "./Testimonials.css"
 
 const NewsLetter = () => {
+
+    const [newsletter, setNewsLetter] = useState("");
+
+    const handleEmail = (e) => {
+        setNewsLetter(e.target.value);
+    }
+
+    const handleNewsletter = () => {
+        setNewsLetter("");
+    }
+
     return (
         <div className="newslatterBackground px-5">
             <div className='2xl:max-w-7xl mx-auto'>
@@ -13,8 +25,8 @@ const NewsLetter = () => {
                     <h3 className='text-sm font-semibold'>Beauty Spa & Salon</h3>
                     <h3 className='text-xl font-semibold mt-8 mb-5'>Subscribe To Our Newsletter</h3>
                     <p className='text-sm mb-5'>If You Have Any Questions, You Can Contact With Us So That We Can Give You A Satisfying Answer. Subscribe <br /> To Our Newsletter To Get Our Latest Products.</p>
-                    <input style={{ borderRadius: "25px" }} type="text" placeholder="Your Email Address" className="input w-full sm:w-5/12 mr-5 contactInput rounded-3xl" />
-                    <button className='btn btn-primary rounded-full text-white font-seimibold capitalize mt-5 sm:mt-0'>Subscribe</button>
+                    <input style={{ borderRadius: "25px" }} type="text" onChange={(e) => handleEmail(e)} value={newsletter} placeholder="Your Email Address" className="input w-full sm:w-5/12 mr-5 contactInput rounded-3xl" />
+                    <button onClick={() => handleNewsletter()} className='btn btn-primary rounded-full text-white font-seimibold capitalize mt-5 sm:mt-0'>Subscribe</button>
                 </div>
             </div>
         </div>
